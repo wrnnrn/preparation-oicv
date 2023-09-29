684a72ac22410b440ba9461c7a542ef019598d60
wrnnrn <74679791+wrnnrn@users.noreply.github.com>
2023-09-29 06:44:39 +0200

    Add files via upload

---
+++ Oicv-5/multiplicaciones.py
@@ -0,0 +1,16 @@
+#Enter of numbers
+multidor = int(input("Multiplicador I: "))
+multiado = int(input("Multiplicador II: "))
+multidos = int(input("Multiplicador III: "))
+
+#Operation
+multiplicado = multidor  * multiado * multidos
+print("El producto de la multiplicacion es: ", multiplicado)
+
+#Explication
+print("El proceso es:")
+print(" ", multidor)
+print(" ", multiado)
+print("x", multidos)
+print(" -------")
+print(" ", multiplicado)
